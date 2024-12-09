316ce34cd2946630f8a72b049ae84cd79a09ec17
Mitchel Mathias <mitchelmathias2904@gmail.com>
2024-12-08 22:20:42 -0300

    Terminando EBAC Motors

---
+++ tarefas/tarefa_09/scripts/main.js
@@ -0,0 +1,61 @@
+/*$(document).ready(function() {
+    if (!interval) { // == if (interval == false || interval == 0 || interval == '' || interval == null || interval == undefined || isNaN(interval))
+        interval = setInterval(teste, 1000); 
+    }
+});
+
+let fotos = $('.carousel img');
+let ini = 0;
+let interval;
+
+function teste() {
+    fotos.hide();  
+    fotos.eq(ini).show();  
+
+    ini++; 
+    if (ini === fotos.length) {
+        ini = 0;  
+    }
+}*/
+
+$(document).ready(function(){
+    $('.carousel').slick({
+        autoplay:true,
+        autoplaySpeed:1000,
+        infinite: true,
+    })
+
+    $('.menu_hamburguer').on('click', function(){
+        $('nav').slideToggle()
+    })
+
+    $('#tel').mask('(00) 00000-0000', {
+        placeholder:'(__) _____-____',
+    })
+
+    $('form').validate({
+        rules:{
+            nome:{
+                required:true,
+            },
+            email:{
+                required: true,
+                email:true,
+            },
+            tel:{
+                required:true,
+            }
+        },
+        messages:{
+            nome:'Digite seu nome'
+        },
+        submitHandler: function(form){
+            alert('Deu certo')
+        },
+        invalidHandler: function(){
+            alert('Erro nos campos')
+        }
+    })
+})
+
+
